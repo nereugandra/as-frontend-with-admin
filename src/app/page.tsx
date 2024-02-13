c185c71011cfbd28154948ac235e4a74c0cfd0d8
nereugandra <gandra.nereu@gmail.com>
2024-02-13 03:40:09 -0300

    commit new-user and credits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 const Page = () => {
   return (
     <div>
+      <h3>Desenvolvido por: Nereu Gandra</h3>
       <h1 className="text-center text-4xl my-3">HOME - Amigo secreto</h1>
     </div>
   );
